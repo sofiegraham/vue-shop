fd652857b4f57c30617780e40f4d0b8436d82aa2
Sofie Graham <sofiegraham@Sofies-MacBook-Pro.local>
2019-09-03 15:42:49 -0600

    Add form submission errors

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 Vue.component('product-review', {
 	template: `
 		<form class="review-form" @submit.prevent="onSubmit">
+
+			<p v-if='errors.length'>
+				<b>Please correct the following:</b>
+				<ul>
+					<li v-for='error in errors'>{{ error }}</li>
+				</ul>
+			</p>
 			<p>
 				<label for="name">Name:</label>
 				<input id="name" v-model="name" placeholder="name">
@@ -28,20 +35,28 @@ Vue.component('product-review', {
 		return {
 			name: null,
 			review: null,
-			rating: null
+			rating: null,
+			errors: []
 		}
 	},
 	methods: {
 		onSubmit() {
-			let productReview = {
-				name: this.name,
-				review: this.review,
-				rating: this.rating
+			this.errors = []
+			if (this.name && this.review && this.rating) {
+				let productReview = {
+					name: this.name,
+					review: this.review,
+					rating: this.rating
+				}
+				this.$emit('review-submitted', productReview)
+				this.name = null,
+				this.review = null,
+				this.rating = null
+			} else {
+				if(!this.name) this.errors.push("Name required")
+				if(!this.review) this.errors.push("Review required")
+				if(!this.rating) this.errors.push("Rating required")
 			}
-			this.$emit('review-submitted', productReview)
-			this.name = null,
-			this.review = null,
-			this.rating = null
 		}
 	}
 })
@@ -103,6 +118,17 @@ Vue.component('product', {
 				<button @click='removeFromCart'
 				:disabled='!isInCart'
 				:class='{ disabledButton: !isInCart }'>Remove from cart</button>
+			</div>
+			<div>
+				<h2>Reviews</h2>
+				<p v-if='!reviews.length'>There are no reviews yet</p>
+				<ul>
+					<li v-for='review in reviews'>
+						<p>{{ review.name }}</p>
+						<p>Rating: {{ review.rating }}</p>
+						<p>{{ review.review }}</p>
+					</li>
+				</ul>
 			</div>
 			<product-review @review-submitted='addReview'></product-review>
 		</div>
